f066f38e00cf937773b8e208094f6a0236cfbf20
Mason Clark <clarkmason34@yahoo.com>
2023-06-12 23:40:48 -0500

    Changes from default to Collection Log KC version of plugin

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Example
-An example greeter plugin
+# Collection Log KC
+This plugin allows you to view the KC that you got a unique drop.
+Note that this only works for uniques that are gotten after the plugin is installed
+(unless Jagex decides to add a way to track before). I may add it to where you can
+add your own KCs for items that were gotten before the plugin was installed.
--- src/test/java/com/logkc/LogKCTest.java
+++ src/test/java/com/logkc/LogKCTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.logkc;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class LogKCTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(LogKCPlugin.class);
 		RuneLite.main(args);
 	}
 }
